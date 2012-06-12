2501e921086ed709e3b676dcb3f2d5fe68913ee4
Sakarin Radchapunya <sakarin.my@gmail.com>
2012-06-13 04:11:49 +0700

    spree 1.1.X support

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -1,154 +1,92 @@
-require "spree_multi_domain"
-
 module SpreeMultiDomain
-
   class Engine < Rails::Engine
+    engine_name 'spree_multi_domain'
+
+    config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
 
-      Spree::BaseController.class_eval do
-        helper_method :current_store
-        helper :products, :taxons
+      Spree::Config.searcher_class = Spree::Search::MultiDomain
+    end
 
-        private
+    config.to_prepare &method(:activate).to_proc
 
-        # Tell Rails to look in layouts/#{@store.code} whenever we're inside of a store (instead of the standard /layouts location)
-        def find_layout(layout, format, html_fallback=false) #:nodoc:
-          layout_dir = current_store ? "layouts/#{current_store.code.downcase}" : "layouts"
-          view_paths.find_template(layout.to_s =~ /\A\/|layouts\// ? layout : "#{layout_dir}/#{layout}", format, html_fallback)
-        rescue ActionView::MissingTemplate
-          raise if Mime::Type.lookup_by_extension(format.to_s).html?
+    initializer "templates with dynamic layouts" do |app|
+      ActionView::TemplateRenderer.class_eval do
+        def find_layout_with_multi_store(layout, locals)
+
+          current_store = @view.controller.send(:current_store) rescue nil
+          store_layout = layout
+
+          if current_store && !@view.controller.is_a?(Spree::Admin::BaseController)
+            store_layout = "layouts/#{current_store.code}/spree_application"
+          end
+
+          begin
+            find_layout_without_multi_store(store_layout, locals)
+          rescue ::ActionView::MissingTemplate
+            find_layout_without_multi_store(layout, locals)
+          end
         end
 
+        alias_method_chain :find_layout, :multi_store
+      end
+    end
+
+    initializer "current order decoration" do |app|
+      ::Spree::Core::CurrentOrder.module_eval do
+        def current_order_with_multi_domain(create_order_if_necessary = false)
+          current_order_without_multi_domain(create_order_if_necessary)
+
+          if @current_order and current_store and @current_order.store.nil?
+            @current_order.update_attribute(:store_id, current_store.id)
+          end
+
+          @current_order
+        end
+        alias_method_chain :current_order, :multi_domain
+      end
+    end
+
+    initializer "controller helpers decoration" do |app|
+      ::Spree::Core::ControllerHelpers.module_eval do
+
         def current_store
-          @current_store ||= ::Store.by_domain(request.env['SERVER_NAME']).first
-          @current_store ||= ::Store.default.first
+          @current_store ||= Spree::Store.current(request.env['SERVER_NAME'])
+        end
+
+        def current_tracker
+          @current_tracker ||= Spree::Tracker.current(request.env['SERVER_NAME'])
         end
 
         def get_taxonomies
-          @taxonomies ||= Taxonomy.find(:all, :include => {:root => :children}, :conditions => ["store_id = ?", @site.id])
+          @taxonomies ||= current_store.present? ? Spree::Taxonomy.where(["store_id = ?", current_store.id]) : Spree::Taxonomy
+          @taxonomies = @taxonomies.find(:all, :include => {:root => :children})
           @taxonomies
         end
 
-      end
-
-      Product.class_eval do
-        has_and_belongs_to_many :stores
-        scope :by_store, lambda {|store| joins(:stores).where("products_stores.store_id = ?", store)}
-      end
-
-      ProductsController.class_eval do
-        before_filter :can_show_product, :only => :show
-
-        private
-        def can_show_product
-         if @product.stores.empty? || @product.stores.include?(@site)
-           render :file => "public/404.html", :status => 404
-         end
+        def add_current_store_id_to_params
+          params[:current_store_id] = current_store.try(:id)
         end
 
-      end
+        class << self
 
-      #override search to make it multi-store aware
-      Spree::Search.module_eval do
-        def retrieve_products
-          # taxon might be already set if this method is called from TaxonsController#show
-          @taxon ||= Taxon.find_by_id(params[:taxon]) unless params[:taxon].blank?
-          # add taxon id to params for searcher
-          params[:taxon] = @taxon.id if @taxon
-          @keywords = params[:keywords]
-          
-          per_page = params[:per_page].to_i
-          per_page = per_page > 0 ? per_page : Spree::Config[:products_per_page]
-          params[:per_page] = per_page
-          params[:page] = 1 if (params[:page].to_i <= 0)
-          
-          # Prepare a search within the parameters
-          Spree::Config.searcher.prepare(params)
+          def included_with_multi_domain(receiver)
+            included_without_multi_domain(receiver)
 
-          if !params[:order_by_price].blank?
-            @product_group = ProductGroup.new.from_route([params[:order_by_price]+"_by_master_price"])
-          elsif params[:product_group_name]
-            @cached_product_group = ProductGroup.find_by_permalink(params[:product_group_name])
-            @product_group = ProductGroup.new
-          elsif params[:product_group_query]
-            @product_group = ProductGroup.new.from_route(params[:product_group_query])
-          else
-            @product_group = ProductGroup.new
+            receiver.send :helper, 'spree/products'
+            receiver.send :helper, 'spree/taxons'
+            receiver.send :before_filter, 'add_current_store_id_to_params'
+            receiver.send :helper_method, 'current_store'
+            receiver.send :helper_method, 'current_tracker'
           end
 
-          @product_group = @product_group.from_search(params[:search]) if params[:search]
-       
-          base_scope = @cached_product_group ? @cached_product_group.products.active : Product.active
-          base_scope = base_scope.by_store(current_store.id) if current_store.present?
-
-          base_scope = base_scope.in_taxon(@taxon) unless @taxon.blank? 
-          base_scope = base_scope.keywords(@keywords) unless @keywords.blank?
-          
-          base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
-          @products_scope = @product_group.apply_on(base_scope)
-
-          curr_page = Spree::Config.searcher.manage_pagination ? 1 : params[:page]
-          @products = @products_scope.all.paginate({
-              :include  => [:images, :master],
-              :per_page => per_page,
-              :page     => curr_page
-            })
-          @products_count = @products_scope.count
-
-          return(@products)
-        end
-      end
-
-      Admin::ProductsController.class_eval do
-        update.before << :set_stores
-
-        create.before << :add_to_all_stores
-
-        private
-        def set_stores
-          @product.store_ids = nil unless params[:product].key? :store_ids
-        end
-
-        def add_to_all_stores
-        end
-      end
-
-      Order.class_eval do
-        belongs_to :store
-        scope :by_store, lambda { |store| where(:store_id => store.id) }
-      end
-
-      Taxonomy.class_eval do
-        belongs_to :store
-      end
-
-      Tracker.class_eval do
-        belongs_to :store
-
-        def self.current
-          trackers = Tracker.find(:all, :conditions => {:active => true, :environment => ENV['RAILS_ENV']})
-          trackers.select { |t| t.store.name == Spree::Config[:site_name] }.first
+          alias_method_chain :included, :multi_domain
         end
       end
     end
-
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.to_prepare &method(:activate).to_proc
-
   end
-
 end
-
-
-Spree::CurrentOrder.module_eval do
-  def current_order_with_multi_domain(create_order_if_necessary = false)
-    current_order_without_multi_domain(create_order_if_necessary)
-    if @current_order and current_store and @current_order.store.nil?
-      @current_order.update_attribute(:store_id, current_store.id)
-    end
-    @current_order
-  end
-  alias_method_chain :current_order, :multi_domain
-end
-
--- spree_multi_domain.gemspec
+++ spree_multi_domain.gemspec
@@ -1,23 +1,27 @@
-version = '3.0.1'
-
+# encoding: UTF-8
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_multi_domain'
-  s.version     = version
+  s.version     = '1.1.1'
   s.summary     = 'Adds multiple site support to Spree'
-  #s.description = 'Email on Rails. Compose, deliver, receive, and test emails using the familiar controller/view pattern. First-class support for multipart email and attachments.'
+  s.description = 'Multiple Spree stores on different domains - single unified backed for processing orders.'
   s.required_ruby_version = '>= 1.8.7'
 
-  # s.author            = 'David Heinemeier Hansson'
-  # s.email             = 'david@loudthinking.com'
-  # s.homepage          = 'http://www.rubyonrails.org'
-  # s.rubyforge_project = 'actionmailer'
+  s.authors           = ['Brian Quinn', 'Roman Smirnov', 'David North']
+  s.email             = 'sakarin.my@gmail.com'
 
-  s.files        = Dir['CHANGELOG', 'README', 'MIT-LICENSE', 'lib/**/*', 'app/**/*', 'config/*']
+  s.rubyforge_project = 'spree_multi_domain'
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  s.has_rdoc = true
+  s.add_dependency 'spree_core', '~> 1.1.1'
 
-  s.add_dependency('spree_core',  '0.30.0.beta1')
+  s.add_development_dependency 'capybara', '1.0.1'
+  s.add_development_dependency 'factory_girl'
+  s.add_development_dependency 'ffaker'
+  s.add_development_dependency 'rspec-rails',  '~> 2.7'
+  s.add_development_dependency 'sqlite3'
 end
