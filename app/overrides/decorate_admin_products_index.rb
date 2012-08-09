429265d37b7ff327f96352b4f8c94f7904089b5f
Gregor MacDougall <gregor@freerunningtechnologies.com>
2012-08-09 15:19:50 -0700

    Removed broken search select.
    Change-Id: Ib76eebbd7ea49501273a41d10e32b02157d58863

--- app/overrides/decorate_admin_products_index.rb
+++ app/overrides/decorate_admin_products_index.rb
@@ -11,10 +11,3 @@ Deface::Override.new(
   :insert_before => "[data-hook='admin_products_index_row_actions']",
   :partial => "spree/admin/products/index_rows",
   :disabled => false)
-  
-Deface::Override.new(
-  :virtual_path => "spree/admin/products/index",
-  :name => "multi_domain_admin_products_index_search",
-  :insert_top => "[data-hook='admin_products_index_search']",
-  :partial => "spree/admin/products/index_search_fields",
-  :disabled => false)
